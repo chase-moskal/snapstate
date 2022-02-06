ca29deab3bad1ba8ed1db035e8d5f8cb68eedea1
Chase Moskal <chasemoskal@gmail.com>
2022-02-05 22:38:55 -0800

    tweak: make unproxy symbol arg optional

--- s/tools/unproxy.ts
+++ s/tools/unproxy.ts
@@ -1,8 +1,9 @@
 
 import {objectMap} from "./object-map.js"
 import {isPlainObject} from "./is-plain-object.js"
+import {symbolToAllowProxyIntoState} from "../snapstate.js"
 
-export function unproxy<X>(x: X, unlessSymbol: symbol): X {
+export function unproxy<X>(x: X, unlessSymbol: symbol = symbolToAllowProxyIntoState): X {
 	return (isPlainObject(x) && !(<any>x)[unlessSymbol])
 		? objectMap(x, value => unproxy(value, unlessSymbol))
 		: x
